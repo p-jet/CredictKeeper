b0207f86953be14453ae745fb761861e2bafb499
pujuntao <pujuntaoo@139.com>
2018-08-07 13:43:57 +0800

    add skin support

--- app/src/main/java/com/melody/creditkeeper/App.java
+++ app/src/main/java/com/melody/creditkeeper/App.java
@@ -1,11 +1,18 @@
 package com.melody.creditkeeper;
 
 import com.melody.base.BaseApplication;
+import com.melody.creditkeeper.utils.SkinUtils;
 
 public class App extends BaseApplication {
 
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+        //初始化皮肤库
+        SkinUtils.getInstance().init(this);
 
+    }
 
 }
